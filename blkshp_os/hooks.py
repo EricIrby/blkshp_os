396ab804ce63cc7d6ec363c97b52ac1ec6b806b4
Eric Irby <eric@eirby.com>
2025-11-09 23:36:31 -0600

    Remove duplicate hooks.py file

--- blkshp_os/hooks.py
+++ blkshp_os/hooks.py
@@ -19,6 +19,20 @@ fixtures = [
 		],
 	},
 	{
+		"dt": "Feature Toggle",
+		"filters": [["feature_key", "in", [
+			"core.workspace.access",
+			"products.bulk_operations",
+			"inventory.audit_workflows",
+			"procurement.ottimate_import",
+			"analytics.finance_dashboard"
+		]]],
+	},
+	{
+		"dt": "Subscription Plan",
+		"filters": [["plan_code", "in", ["FOUNDATION"]]],
+	},
+	{
 		"dt": "Module Activation",
 		"filters": [["plan", "in", ["FOUNDATION"]]],
 	},
